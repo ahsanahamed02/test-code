d3a00a0039402c1682b3b047ef5f9e52e46fd2d6
R.Ahsan Ahamed <rifasahsanahamed02@gmail.com>
2025-06-20 17:43:54 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import random
+quotes = [
+    "Code is like humor. When you have to explain it, it’s bad.",
+    "Keep it simple, Ahsan Ahamed!",
+    "First, solve the problem. Then, write the code.",
+]
+
+print("🧠 Ahsan Ahamed's Python Console App")
+print("💬 Quote of the Day:", random.choice(quotes))
