954fc65d51265c77c6702a135bbc8bdde161b182
R.Ahsan Ahamed <rifasahsanahamed02@gmail.com>
2025-06-20 17:42:37 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "🚀 Welcome to Ahsan Ahamed's C++ World!" << endl;
+    cout << "Let's build something awesome 💡" << endl;
+    return 0;
+}
